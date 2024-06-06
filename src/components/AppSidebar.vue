ec10a61b410c53f39be9090be2719a00c3469ce5
MaoRusk <manolo_rod.98@hotmail.com>
2024-06-06 16:46:20 -0600

    Logos fixed

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -21,8 +21,8 @@ const sidebar = useSidebarStore()
     <CSidebarHeader class="border-bottom">
       <RouterLink custom to="/" v-slot="{ href, navigate }">
         <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          <CIcon custom-class-name="sidebar-brand-full" :icon="logo" :height="32" />
-          <CIcon custom-class-name="sidebar-brand-narrow" :icon="sygnet" :height="32" />
+          <CIcon custom-class-name="sidebar-brand-full" :icon="logo" :height="50" />
+          <CIcon custom-class-name="sidebar-brand-narrow" :icon="sygnet" :height="50" />
         </CSidebarBrand>
       </RouterLink>
       <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
