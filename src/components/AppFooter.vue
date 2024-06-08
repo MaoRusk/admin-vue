e73611f25387b59416f0ec7e93a77e135d7446d3
MiriamHerrera <53792107+MiriamHerrera@users.noreply.github.com>
2024-06-07 21:04:08 -0600

    Polizas v1

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,12 @@
 <template>
-  <CFooter class="px-4">
-    <div>
+  <CFooter style="background: rgb(243 244 247 / 0%);" class="px-4">
+    <!-- <div>
       <a href="https://coreui.io" target="_blank">CoreUI</a>
       <span class="ms-1">&copy; {{ new Date().getFullYear() }} creativeLabs.</span>
     </div>
     <div class="ms-auto">
       <span class="me-1" target="_blank">Powered by</span>
       <a href="https://coreui.io/vue">CoreUI for Vue</a>
-    </div>
+    </div> -->
   </CFooter>
 </template>
