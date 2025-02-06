2a17e2a22810ba9586ec6c2d0e7e754065de0252
MiriamHerrera <53792107+MiriamHerrera@users.noreply.github.com>
2025-02-06 11:20:42 -0600

    limpiar plantilla v1

--- src/components/AppHeaderDropdownAccnt.vue
+++ src/components/AppHeaderDropdownAccnt.vue
@@ -23,43 +23,11 @@ const logout = () => {
       <CAvatar size="md" color="success">A</CAvatar>
     </CDropdownToggle>
     <CDropdownMenu class="pt-0">
-      <CDropdownHeader
-        as="h6"
-        class="bg-body-secondary text-body-secondary fw-semibold rounded-top mb-2"
-      >
-        {{ t('account') }}
-      </CDropdownHeader>
-      <CDropdownItem>
-        <CIcon icon="cil-bell" /> {{ t('updates') }}
-        <CBadge color="info-gradient" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
-      <CDropdownItem>
-        <CIcon icon="cil-envelope-open" /> {{ t('messages') }}
-        <CBadge color="success-gradient" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
-      <CDropdownItem>
-        <CIcon icon="cil-task" /> {{ t('tasks') }}
-        <CBadge color="danger-gradient" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
-      <CDropdownItem>
-        <CIcon icon="cil-comment-square" /> {{ t('comments') }}
-        <CBadge color="warning-gradient" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
+    
       <CDropdownHeader as="h6" class="bg-body-secondary text-body-secondary fw-semibold my-2">
         {{ t('settings') }}
       </CDropdownHeader>
       <CDropdownItem> <CIcon icon="cil-user" /> {{ t('profile') }} </CDropdownItem>
-      <CDropdownItem> <CIcon icon="cil-settings" /> {{ t('settings') }} </CDropdownItem>
-      <CDropdownItem>
-        <CIcon icon="cil-dollar" /> {{ t('payments') }}
-        <CBadge color="secondary" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
-      <CDropdownItem>
-        <CIcon icon="cil-file" /> {{ t('projects') }}
-        <CBadge color="primary-gradient" class="ms-auto">{{ itemsCount }}</CBadge>
-      </CDropdownItem>
-      <CDropdownDivider />
-      <CDropdownItem> <CIcon icon="cil-shield-alt" /> {{ t('lockAccount') }} </CDropdownItem>
       <CDropdownItem @click="logout"> <CIcon icon="cil-lock-locked" /> {{ t('logout') }} </CDropdownItem>
     </CDropdownMenu>
   </CDropdown>
